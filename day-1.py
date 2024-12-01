059e6eae2804e56775c374aef84d925f773dcfe8
Keerthana Sandhiri <97373191+psykeefuego@users.noreply.github.com>
2024-12-01 19:21:07 +0530

    Update day-1.py

--- day-1.py
+++ day-1.py
@@ -1,11 +1,29 @@
 # Historian Hysteria 
 
-n1,n2=map(int,input().split())
-l1=[int(digit) for digit in str(n1)]
-l2=[int(digit) for digit in str(n2)]
-l1.sort()
-l2.sort()
-ans=0
-for i in range(len(l1)):
-  ans+=abs(l1[i]-l2[i])
-print(ans)
+import sys
+from collections import defaultdict, Counter
+infile = sys.argv[1] if len(sys.argv)>=2 else '1.in'
+D = open(infile).read().strip()
+
+lines = D.split('\n')
+LEFT = []
+RIGHT = []
+RC = Counter()
+
+for line in lines:
+    l,r = line.split()
+    l,r = int(l),int(r)
+    LEFT.append(l)
+    RIGHT.append(r)
+    RC[r] += 1
+p1 = 0
+LEFT = sorted(LEFT)
+RIGHT = sorted(RIGHT)
+for l,r in zip(LEFT,RIGHT):
+    p1 += abs(r-l)
+print(p1)
+
+p2 = 0
+for l in LEFT:
+    p2 += l * RC.get(l,0)
+print(p2)
