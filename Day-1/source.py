06fb512f2b56577038546187a527610a7c03ce93
Keerthana Sandhiri <97373191+psykeefuego@users.noreply.github.com>
2024-12-01 19:50:00 +0530

    Update source.py

--- Day-1/source.py
+++ Day-1/source.py
@@ -1,13 +1,13 @@
 # Historian Hysteria
-
-
 from collections import Counter
 
 with open("input.txt") as f:
     l=[[int(i) for i in l.split()] for l in f]
 
+# part 1
 l1,l2=[list(sorted(x)) for x in zip(*l)]
 print(sum(abs(a-b) for a, b in zip(l1, l2)))
 
+# part 2
 ctr=Counter(l2)
 print(sum(i*ctr[i] for i in l1))
